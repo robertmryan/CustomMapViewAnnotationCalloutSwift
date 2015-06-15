bc63713baefbbaf22335963d39508c25615abc1d
Rob Ryan <robert.ryan@mindspring.com>
2015-06-15 01:25:18 -0400

    Initial Commit

---
+++ SwiftMapViewCustomCallout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMapViewCustomCallout
+//
+//  Created by Robert Ryan on 6/15/15.
+//  Copyright (c) 2015 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
